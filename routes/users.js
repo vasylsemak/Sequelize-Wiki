293ada00e025e9952801ebdca936f28d6c4ae903
Vasyl Semak <vasylsmk1401@gmail.com>
2021-02-11 20:44:37 -0500

    add: route to get all articles by author

--- routes/users.js
+++ routes/users.js
@@ -1,14 +1,24 @@
 const router = require('express').Router()
 module.exports = router
 
-const { User } = require('../models')
+const { User, Page } = require('../models')
 const userList = require('../views/userList')
+const userPages = require('../views/userPages')
 
 router.get('/', async(req, res, next) => {
   try {
     const users = await User.findAll()
     res.send(userList(users))
-  } catch(error) {
-    next(error)
-  }
+  } catch(error) { next(error) }
+})
+
+router.get('/:id', async(req, res, next) => {
+  try {
+    const user = await User.findByPk(req.params.id)
+    const pages = await Page.findAll({
+      where: { authorId: user.id }
+    })
+
+    res.send(userPages(user, pages))
+  } catch(error) { next(error) }
 })
